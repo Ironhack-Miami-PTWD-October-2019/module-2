026e587e50a256059e5a4250df7676a60f962e84
sandrabosk <sandraboskovic@hotmail.com>
2020-01-27 17:57:56 -0500

    add solution of extra iterations of es6 reinforcement lab

--- lab-es6-class-review.js
+++ lab-es6-class-review.js
@@ -178,6 +178,82 @@ for (const user of usersArray) {
   // userCreditDetails.push(data);
 }
 
+// console.log('userCreditDetails', userCreditDetails);
+
+// Task 5:
+// Create a function `genderView` which will return two arrays new arrays femaleUsers and maleUsers in a object
+// depending on the gender fill them with strings containing users' first and last names
+
+function genderView(users) {
+  const femaleUsers = [];
+  const maleUsers = [];
+  users.filter(user => {
+    if (user.gender === 'female') femaleUsers.push(`${user.firstName} ${user.lastName}`);
+    else maleUsers.push(`${user.firstName} ${user.lastName}`);
+  });
+  return {
+    femaleUsers,
+    maleUsers
+  };
+}
+
+// genderView (usersArray)
+
+// Task 6:
+// Use the returned object from the previous function and print how many female and how many male users there are.
+
+const data = genderView(usersArray);
+
+// console.log(data);
+
+function genderCounts(someObject) {
+  // console.log(Object.keys(someObject));
+  //  console.log(someObject);
+  const females = someObject.femaleUsers;
+  const males = someObject.maleUsers;
+  for (let key of Object.keys(someObject)) {
+    // console.log(key)
+    if (key === 'femaleUsers') console.log(`There are ${females.length} female users.`);
+    else console.log(`There are ${males.length} female users.`);
+  }
+}
+
+// genderCounts(data);
+
+// Task 7:
+// Find all users whose balance is greater then 20000 and send them personalized message offering some promotion
+
+function promo20(users) {
+  return users
+    .filter(user => {
+      let { balance } = user;
+      // console.log(typeof user.balance)
+      let cleaned = +balance.substring(1).split(',')[0];
+      // console.log(typeof cleaned)
+      if (cleaned > 20) return user;
+    })
+    .map(user =>
+      console.log(
+        `Dear ${user.firstName}, since your balance is ${user.balance}, you are eligible to apply for this awesome credit card.`
+      )
+    );
+}
+
+// promo20(usersArray)
+
+// Task 8:
+// spread
+// Loop through usersArray and make all users active (use ... with objects 😉)
+
+const allActive = usersArray.map(user => {
+  return {
+    ...user,
+    isActive: true
+  };
+});
+
+console.log(allActive);
+
 // *******************************************************************************************
 // SIDE NOTE: HOW WE CAN NAME PROPERTIES OF A NEWLY CREATED OBJECT
 
