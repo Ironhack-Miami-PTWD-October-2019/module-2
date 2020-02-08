67132a4ef2d5091d0327b04da934b1cd4ecc2e64
sandrabosk <sandraboskovic@hotmail.com>
2020-02-08 15:15:23 -0500

    add bcrypt from wednesday and basic auth from saturday

---
+++ basicAuth/routes/index.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const routeGuard = require('../configs/route-guard.config');
+
+/* GET home page */
+router.get('/', (req, res) => {
+  if (req.session.visitCount) {
+    req.session.visitCount += 1;
+  } else {
+    req.session.visitCount = 1;
+  }
+  res.render('index', {
+    title: 'App created with Ironhack generator 🚀',
+    visitCount: req.session.visitCount,
+    sess: JSON.stringify(req.session)
+  });
+});
+
+router.get('/secret', routeGuard, (req, res) => res.render('users/secret'));
+
+module.exports = router;
---
+++ bcryptDemo/app.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+const plainPassword1 = 'HelloWorld';
+const plainPassword2 = 'helloworld';
+
+const salt = bcrypt.genSaltSync(saltRounds);
+
+console.log(`Salt => ${salt}`);
+
+const hash1 = bcrypt.hashSync(plainPassword1, salt);
+const hash2 = bcrypt.hashSync(plainPassword2, salt);
+
+const arePasswordsTheSame = bcrypt.compareSync(plainPassword1, hash1);
+
+console.log(`Hash 1: ${hash1}`);
+console.log(`Hash 2: ${hash2}`);
+console.log(`arePasswordsTheSame: ${arePasswordsTheSame}`);
+console.log('----------------------------------------');
